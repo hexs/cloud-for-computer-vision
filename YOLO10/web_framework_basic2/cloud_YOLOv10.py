b2581f6cdc3c81dee7278d60cb110a2373b303d1
j500 <C026730@cht-dm.cht.canon.co.th>
2024-07-29 20:00:04 +0700

    update web_framework_basic2

--- YOLO10/web_framework_basic2/cloud_YOLOv10.py
+++ YOLO10/web_framework_basic2/cloud_YOLOv10.py
@@ -2,13 +2,37 @@
 # แต่ข้อมูลที่ตอบกลับเป็นข้อมูลเก่าที่ประมวลผลเก็บไว้
 
 import json
+import os
 import urllib.request
 from datetime import datetime
+import socket
+from urllib.parse import urlparse
 from ultralytics import YOLO
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import cv2
 import numpy as np
 from multiprocessing import Process, Manager
+
+
+def update_config_file(filename, updates):
+    data = {}
+    if filename in os.listdir():
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        data.update(updates)
+
+    if data.get('ipv4') is None:
+        hostname = socket.gethostname()
+        ipv4 = socket.gethostbyname(hostname)
+        data.update({'ipv4': ipv4})
+    if data.get('port') is None:
+        data.update({'port': '1000'})
+
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+    print(data)
+    return data
+
 
 # Load the YOLO model
 model = YOLO("../yolov10n.pt")
@@ -18,49 +42,73 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return (
+        "<a href='/config'>config</a><br>"
+        "<a href='/get-detections'>get-detections</a><br>"
+    )
 
 
-@app.route("/predict")
-def predict():
-    data = app.config['data']
-    detections = data['detections']
+@app.route('/config', methods=['GET', 'POST'])
+def config():
+    if request.method == 'POST':
+        ipv4 = request.form.get('ipv4')
+        port = request.form.get('port')
+        other = request.form.get('other')
+
+        app.config['data']['url_image'] = f"http://{ipv4}:{port}/{other}"
+        print(f"url = http://{ipv4}:{port}/{other}")
+
+        return f"http://{ipv4}:{port}/{other}"
+
+    parsed_url = urlparse(app.config['data']['url_image'])
+    ipv4 = parsed_url.hostname
+    port = parsed_url.port
+    return render_template('config.html', default_ipv4=ipv4, default_port=port)
+
+
+@app.route("/get-detections")
+def get_detections():
+    detections = app.config['data']['detections']
     return json.dumps(detections)
 
 
-def get_image(data):
-    url = f"http://{data['client_ipv4'][0]}:{data['client_ipv4'][1]}/image"
+def predict(data):
     while True:
-        t1 = datetime.now()
-        try:
-            req = urllib.request.urlopen(url)
-            arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-            img = cv2.imdecode(arr, -1)
+        if data['url_image']:
+            t1 = datetime.now()
+            try:
+                req = urllib.request.urlopen(data['url_image'])
+                arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+                img = cv2.imdecode(arr, -1)
 
-            results = model(img)
-            detections = results[0].boxes.data.tolist()
-        except Exception as e:
-            print(f"Error: {e}")
-            detections = []
-        data['detections'] = detections
-        t2 = datetime.now()
-        time_diff = (t2 - t1).total_seconds()
-        print(f"Processing time: {time_diff:.2f} seconds")
+                results = model(img)
+                detections = results[0].boxes.data.tolist()
+
+            except Exception as e:
+                print(f"Error: {e}")
+                detections = []
+            data['detections'] = detections
+            t2 = datetime.now()
+            time_diff = (t2 - t1).total_seconds()
+            print(f"Processing time: {time_diff:.2f} seconds")
 
 
 def run_server(data):
     app.config['data'] = data
-    app.run(*data['server_ipv4'], debug=True, use_reloader=False)
+    app.run(data['ipv4'], data['port'], debug=True, use_reloader=False)
 
 
 if __name__ == "__main__":
+    config = update_config_file('config.json', {})
+
     manager = Manager()
     data = manager.dict()
     data['detections'] = []
-    data['client_ipv4'] = '192.168.137.1', 2000
-    data['server_ipv4'] = '192.168.137.1', 1000
+    data['ipv4'] = f"{config['ipv4']}"
+    data['port'] = f"{config['port']}"
+    data['url_image'] = None
 
-    get_image_process = Process(target=get_image, args=(data,))
+    get_image_process = Process(target=predict, args=(data,))
     run_server_process = Process(target=run_server, args=(data,))
 
     get_image_process.start()
--- YOLO10/web_framework_basic2/device.py
+++ YOLO10/web_framework_basic2/device.py
@@ -1,42 +1,21 @@
 import json
+import urllib.request
 from datetime import datetime
 import cv2
 import numpy as np
 import requests
-from multiprocessing import Process, Manager
-from flask import Flask, Response
-
-app = Flask(__name__)
 
 
-@app.route('/image')
-def get_image():
-    frame = app.config['data']['img']
-    success = app.config['data']['status']
-    if not success:
-        frame = np.full([500, 500, 3], [200, 200, 200], np.uint8)
-        cv2.putText(frame, 'Failed to capture image', (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.putText(frame, 'from camera 0', (30, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.putText(frame, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), (30, 130), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                    (0, 0, 255), 2)
-    ret, buffer = cv2.imencode('.jpg', frame)
-    return Response(buffer.tobytes(), mimetype='image/jpeg')
+def video_capture():
 
-
-def video_capture(data):
-    cap = cv2.VideoCapture(0)
-    url = f"http://{data['server_ipv4'][0]}:{data['server_ipv4'][1]}/predict"
+    url = f"http://192.168.225.137:1000/get-detections"
     while True:
         t1 = datetime.now()
-        status, img = cap.read()
-        data['status'] = status
-        if status:
-            data['img'] = img.copy()
-        else:
-            cap = cv2.VideoCapture(0)
-            data['img'] = np.full((480, 640, 3), (50, 50, 50), dtype=np.uint8)
 
-        frame = data['img']
+        req = urllib.request.urlopen('http://192.168.225.137:2000/image?source=video_capture&id=0')
+        arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+        frame = cv2.imdecode(arr, -1)
+
         response = requests.get(url)
         if response.status_code == 200:
             detections = json.loads(response.text)
@@ -56,24 +35,7 @@ def video_capture(data):
             break
 
 
-def run_server(data):
-    app.config['data'] = data
-    app.run(*data['client_ipv4'], debug=True, use_reloader=False)
 
 
 if __name__ == "__main__":
-    manager = Manager()
-    data = manager.dict()
-    data['client_ipv4'] = '192.168.137.1', 2000
-    data['server_ipv4'] = '192.168.137.1', 1000
-    data['img'] = None
-    data['status'] = False
-
-    capture_process = Process(target=video_capture, args=(data,))
-    run_server_process = Process(target=run_server, args=(data,))
-
-    capture_process.start()
-    run_server_process.start()
-
-    capture_process.join()
-    run_server_process.join()
+    video_capture()
